c361ad16fe1ee86ea4ce9705a9ff52381e3c7724
Mike Dark <agyemankojo47@gmail.com>
2023-06-23 23:29:03 +0000

    Correct Linters Err

--- index.js
+++ index.js
@@ -143,9 +143,9 @@ const projectPopupWindow = [
   },
   {
     id: '5',
-    image: '/Images/SnapshootF-Portfolio.png',
+    image: '/Images/Snapshoot-Portfolio.png',
     alt: 'Popup Window Image',
-    heading: 'Keeping track of hundreds of components',
+    heading: 'Keeping ',
     technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'Bootstrap', 'Terminal'],
     paragraph:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesettin industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.',
